038b11b8d22e653321852f7680eea5a1eef9b30e
minji Kim <sophiaminji@gmail.com>
2020-04-25 20:17:48 +0200

    commit

---
+++ packages/my_package/src/drive_circle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import os
+import numpy as np
+import cv2
+import sys
+import rospy
+import math
+import time
+from duckietown import DTROS
+from std_msgs.msg import String
+from sensor_msgs.msg import CompressedImage, CameraInfo, Image
+from cv_bridge import CvBridge, CvBridgeError
+from image_geometry import PinholeCameraModel
+from duckietown_msgs.msg import Twist2DStamped
+
+class DriveCircle(DTROS):
+    def __init__(self, node_name):
+    # initialize the DTROS parent class
+    super(DriveCircle, self).__init__(node_name=node_name)
+    
+    self.pub_move = rospy.Publisher('/joy_mapper_node/car_cmd', Twist2DStamped, queue_size = 1)
+
+
+if __name__ == '__main__':
+    # create the node
+    node = DriveCircle(node_name='drive_circle')
+    # run node
+    #node.run()
+    # keep spinning
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("shutting down")
+
+    cv2.destroyAllWindows()
---
+++ packages/my_package/src/network.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import os
+import numpy as np
+import cv2
+import sys
+import rospy
+import math
+import time
+from duckietown import DTROS
+from std_msgs.msg import String
+from sensor_msgs.msg import CompressedImage, CameraInfo, Image
+from cv_bridge import CvBridge, CvBridgeError
+from image_geometry import PinholeCameraModel
+from duckietown_msgs.msg import Twist2DStamped
+
+class Network(DTROS):
+    def __init__(self, node_name):
+    # initialize the DTROS parent class
+    super(Network, self).__init__(node_name=node_name)
+
+if __name__ == '__main__':
+    # create the node
+    node = Network(node_name='network')
+
+    # keep spinning
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("shutting down")
+
+    cv2.destroyAllWindows()
